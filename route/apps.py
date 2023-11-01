3dd63dd70c0cbdde53383e79f43bedaea7c82561
Mykyta Havrylenko <nikitagawrilenko2212@gmail.com>
2023-11-01 07:11:26 +0200

    A fix using black.

--- route/apps.py
+++ route/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RouteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'route'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "route"
